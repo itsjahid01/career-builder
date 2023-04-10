ad08a4fe087cf3b37ba3a42d3db6fbe29f7334cf
itsjahid01 <jahidnoman0@gmail.com>
2023-04-11 03:46:14 +0600

    job category list done

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,7 +4,7 @@ const Category = (props) => {
   const { jobCategoryLogo, categoryName, availableJobs } = props.category;
   //   console.log(props);
   return (
-    <div className="bg-[#FAF8FF] p-5 rounded">
+    <div className="bg-[#FAF8FF] p-5 rounded-md">
       <div className="bg-[#EDEEFF] mb-3 p-3 w-20 rounded-md ">
         <img src={jobCategoryLogo} alt="" className="mx-auto" />
       </div>
