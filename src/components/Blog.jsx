f3517f6e81769684cde9212a18646ba792b9e494
itsjahid01 <jahidnoman0@gmail.com>
2023-04-10 16:31:06 +0600

    header done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Blog = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <p>Blogs</p>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Statistics = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="bg-[#F9F8FF] text-lg text-center font-bold p-7 ">
+        <p>Statistics</p>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
